f6380d376959eadc177bede4be98f73102818abc
Juan Lara <max_lara3005@hotmail.com>
2022-08-22 15:11:58 -0500

    Version Final API

--- app.js
+++ app.js
@@ -1,39 +1,47 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
 
-const api = process.env.API_URL;
-
-//Middleware
 app.use(cors());
-app.options('*', cors());
-app.use(express.json());
-//Middleware to log server events like requests.
+app.options('*', cors())
+
+//middleware
+app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
+
 //Routes
-const productsRouter = require('./routers/products');
-app.use(`${api}/products`, productsRouter);
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
-const categoryRouter = require('./routers/categories');
-app.use(`${api}/categories`, categoryRouter);
+const api = process.env.API_URL;
 
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
 mongoose.connect(process.env.CONNECTION_STRING, {
-    useNewUrlParser:true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: 'eshop-database'
 })
-.then(() => {
-    console.log("Database Connection is ready...");
+.then(()=>{
+    console.log('Database Connection is ready...')
 })
-.catch((err) => {
+.catch((err)=> {
     console.log(err);
 })
 
+//Server
 app.listen(3000, ()=>{
-    console.log(api);
-    console.log("The server is runnin http://localhost:3000")
-});
+
+    console.log('server is running http://localhost:3000');
+})
--- models/category.js
+++ models/category.js
@@ -1,17 +1,25 @@
-const mongoose  = require("mongoose");
-const { Schema } = mongoose;
-
+const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
-    categoryName:{
-        type: String
+    name: {
+        type: String,
+        required: true,
     },
-    icon : {
-        type: String
+    icon: {
+        type: String,
     },
-    color: {
-        type: String
+    color: { 
+        type: String,
     }
+})
+
+
+categorySchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+categorySchema.set('toJSON', {
+    virtuals: true,
 });
 
 exports.Category = mongoose.model('Category', categorySchema);
