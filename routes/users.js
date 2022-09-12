e87e09ed8849767bf1b52f95f837cd4479832f6b
Juan ramon Lara montoya <jlara@MacBook-Pro-de-Juan.local>
2022-09-11 20:56:19 -0500

    updating users api

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 router.get(`/`, async (req, res) =>{
     const userList = await User.find();
@@ -9,6 +10,63 @@ router.get(`/`, async (req, res) =>{
         res.status(500).json({success: false})
     } 
     res.send(userList);
+})
+
+router.get('/:id', async(req,res) => {
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if(!user){
+        res.status(500).json({message: 'The user with the given ID was not found'});
+    }
+    res.status(200).send(user);
+});
+
+router.post('/', (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    });
+    
+    user.save();
+
+    if(!user){
+        return res.status(400).send('the user cannot be created!!');
+    }
+
+    res.send(user);
+
+});
+
+router.put('/:id',async (req, res)=> {
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: bcrypt.hashSync(req.body.password, 10),
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country,
+        },
+        { new: true}
+    )
+
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
 })
 
 module.exports =router;
